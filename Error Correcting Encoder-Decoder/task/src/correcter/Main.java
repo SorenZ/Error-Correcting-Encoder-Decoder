e8badc7f00c1ae9b1d612827c6e4a0b3e7e5cd94
Hamed ZVand <sunrise.freedom@gmail.com>
2020-11-01 20:52:38 +0330

    Symbol-level error emulator

--- Error Correcting Encoder-Decoder/task/src/correcter/Main.java
+++ Error Correcting Encoder-Decoder/task/src/correcter/Main.java
@@ -1,7 +1,40 @@
 package correcter;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+        char[] textArray = text.toCharArray();
+
+        for (int i = 0; i < textArray.length - 1 ; i += 3) {
+            var upper = Math.min(textArray.length - 1, i + 2);
+            var randomIndex = getRandom(i, upper);
+
+            textArray[randomIndex] = getRandomChar(textArray[randomIndex]);
+        }
+
+        System.out.println(textArray);
+    }
+
+    private static char getRandomChar(char forbiddenChar)
+    {
+        Random r = new Random();
+        char c = ' ';
+        do {
+            c = (char) (r.nextInt('Z' - 'A') + 'A');
+        } while (c == forbiddenChar);
+
+        return c;
+    }
+
+    private static int getRandom(int lower, int upper)
+    {
+        Random random = new Random();
+        return random.nextInt(upper - lower + 1) + lower;
     }
 }
